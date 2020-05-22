01a517df462d4f91eaad9ab84c52378eedb8d430
Phillip Krawetz <phillip.kraw@gmail.com>
2020-05-23 05:50:26 +0900

    remove broken using statement

--- MediaPlayer.Domain/Models/Tag.cs
+++ MediaPlayer.Domain/Models/Tag.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Avalonia.Media;
-using MediaPlayer.Domain.Interfaces;
 
 namespace MediaPlayer.Domain.Models
 {
